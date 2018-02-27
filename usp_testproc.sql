606839bdcdb86aedd2fb5a6e28bcd5f85f6f61dc
kpadmanabhan <kannan.padmanabhan@ticketmaster.com>
2018-02-27 06:17:48 -0600

    Test stored procedure added

---
+++ usp_testproc.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[TEST_PROC]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	DECLARE @ctr INT = 0
+	PRINT 'This is not working'
+END
