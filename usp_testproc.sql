69500dbd14e27020da343a49282cb8b472b8548d
kpadmanabhan <kannan.padmanabhan@ticketmaster.com>
2018-02-28 12:47:39 -0600

    added another print statement to stored proc.

---
+++ usp_testproc.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[TEST_PROC]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	DECLARE @ctr INT = 0
+	PRINT 'This sworking'
+	PRINT 'This is changed'
+END
